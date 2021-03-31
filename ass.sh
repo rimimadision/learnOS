f6dd5ac94c641be97cc4a11ef485789cb79bb7bd
rimimadision <1550864218@qq.com>
2021-03-31 07:22:49 -0700

    add kernel loader

--- ass.sh
+++ ass.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-nasm -o bin/mbr.bin mbr.S
+nasm -I include/ -o ./bin/mbr.bin mbr.S
+nasm -I include/ -o ./bin/loader.bin loader.S
+
 cd /usr/local/bochs/bin/
+
 sudo dd if=~/os/learnOS/bin/mbr.bin of=./60M.img bs=512 count=1 conv=notrunc
+sudo dd if=~/os/learnOS/bin/loader.bin of=./60M.img bs=512 count=1 seek=2 conv=notrunc
 sudo bochs -f bochsrc.disk
