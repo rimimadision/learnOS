600da571a826ccd003904f369a1ef4e331b18fc2
rimimadision <1550864218@qq.com>
2021-04-06 05:07:07 -0700

    Add enter into p_mode

--- asm.sh
+++ asm.sh
@@ -6,5 +6,5 @@ nasm -I include/ -o ./bin/loader.bin loader.S
 cd /usr/local/bochs/bin/
 
 sudo dd if=~/os/learnOS/bin/mbr.bin of=./60M.img bs=512 count=1 conv=notrunc
-sudo dd if=~/os/learnOS/bin/loader.bin of=./60M.img bs=512 count=1 seek=2 conv=notrunc
+sudo dd if=~/os/learnOS/bin/loader.bin of=./60M.img bs=512 count=4 seek=2 conv=notrunc
 sudo bochs -f bochsrc.disk
