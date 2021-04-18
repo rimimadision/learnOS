aef33cbebe4b7f76cbb35f4f86304a5a4f94b720
rimimadision <1550864218@qq.com>
2021-04-18 00:33:25 -0700

    Complte put_str and put_int

--- lib/kernel/main.c
+++ lib/kernel/main.c
@@ -1,17 +1,10 @@
 #include "print.h"
 void main(void)
 {
-	put_char('k');
-	put_char('e');
-	put_char('r');
-	put_char('n');
-	put_char('e');
-	put_char('l');
+	put_str("I am ironman");
+	put_int(0);
 	put_char('\n');
-	put_char('1');
-	put_char('2');
-	put_char('\b');
-	put_char('3');
+	put_int(0x123a);
 	while(1);
 	return;
 }
