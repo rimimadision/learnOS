4fe8cd0ce90d67c3a05f83fb8cea5e487ae81ffe
rimimadision <1550864218@qq.com>
2021-05-31 11:15:52 -0700

    Add string

--- lib/string.c
+++ lib/string.c
@@ -1,3 +1,46 @@
 #include "string.h"
 #include "global.h"
-#include "debug.h"
+#include "string.h"
+
+/* set total "size" bytes "value" from "dst_"*/
+void memset(void* dst_, uint8_t value, uint32_t size)
+{
+	ASSERT(dst_ != NULL);
+	uint8_t dst_bytes = (uint8_t)dst_;
+ 	while(size--)
+ 	{
+		*dst_bytes++ = value;
+	}
+}
+ 
+/* copy total "size" bytes from dst_ to src_ */
+void memcpy(void* dst_, const void* src_, uint32_t size)
+{
+	ASSERT(dst_ != NULL && src_ != NULL);
+	uint8_t dst_bytes = (uint8_t)dst_;
+	const uint8_t src_bytes = (const uint8_t)src_;
+
+	while(size--)
+	{   
+	   *dst_bytes++ = *src_bytes;
+	}
+}   
+ 
+/*compare total "size" bytes between  a_ and b_, 
+  if equal, return 0,
+  if a_ > b_, return 1,
+  if a_ < b_, return -1*/
+int8_t memcmp(const void* a_, const void* b_, uint32_t size)
+{   
+	ASSERT(a_ != NULL && b_ != NULL);
+	const char* a_bytes = a_;
+  	const char* b_bytes = b_;
+}   
+ 
+void strcpy(char* dst_, const char* src_);
+void strlen(const char* str); 
+int8_t strcmp(const char* aStr, const char* bStr);
+char* strchr(const char* str, const char ch);
+char* strrchr(const char* str, const char ch);
+char* strcat(char* dst_, const char* src_);
+uint32_t strchrs(const char* str, uint8_t ch);
--- lib/string.h
+++ lib/string.h
@@ -1,4 +1,17 @@
 #ifndef __LIB_STRING_H
 #define __LIB_STRING_H
 
-void memset(void* dst_, );
+/* function about manipulating memory */
+void memset(void* dst_, uint8_t value, uint32_t size);
+void memcpy(void* dst_, const void* src_, uint32_t size);
+int8_t memcmp(const void* dst_, const void* src_, uint32_t size);
+
+/* function about manipulating string */
+void strcpy(char* dst_, const char* src_);
+void strlen(const char* str);
+int8_t strcmp(const char* aStr, const char* bStr);
+char* strchr(const char* str, const char ch);
+char* strrchr(const char* str, const char ch);
+char* strcat(char* dst_, const char* src_);
+uint32_t strchrs(const char* str, uint8_t ch);
+#endif // __LIB_STRING_H
