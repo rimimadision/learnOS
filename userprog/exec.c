53f948ced0c467dc4713dcc06094c49727c3a933
rimimadision <1550864218@qq.com>
2021-09-21 05:39:56 -0700

    Add ELF header

--- userprog/exec.c
+++ userprog/exec.c
@@ -1,1 +1,28 @@
+#include "exec.h"
+#include "stdint.h"
 
+extern void intr_exit(void);
+typedef uint32_t Elf32_Word, Elf32_Addr, Elf32_Off;
+typedef uint16_t Elf32_Half;
+
+struct Elf32_Ehdr {
+	unsigned char e_ident[16];
+	Elf32_Half e_type;
+	Elf32_Half e_machine;
+	Elf32_Word e_version;
+	Elf32_Addr e_entry;
+	Elf32_Off e_phoff;
+	Elf32_Off e_shoff;
+	Elf32_Word e_flags;
+	Elf32_Half e_ehsize;
+	Elf32_Half e_phentsize;
+	Elf32_Half e_phnum;
+	Elf32_Half e_shensize;
+	Elf32_Half e_shnum;
+	Elf32_Half e_shstrndx;	 
+};
+
+struct Elf32_Phdr {
+	
+
+};
---
+++ userprog/exec.h
@@ -0,0 +1,8 @@
+#ifndef __USERPROG_EXEC_H
+#define __USERPROG_EXEC_H
+
+
+
+
+
+#endif // __USERPROG_EXEC_H
